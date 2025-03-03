feb7e69aa4d2fd562ad63b0524662633cd52a944
Yo Kobayashi <82624334+KobayashiYoh@users.noreply.github.com>
2025-03-03 20:56:55 +0900

    ローカルにsa-keyをダウンロードする処理を追加

--- modules/iam/main.tf
+++ modules/iam/main.tf
@@ -19,4 +19,10 @@ resource "google_project_iam_member" "terraform_sa_roles" {
 resource "google_service_account_key" "terraform_sa_key" {
   service_account_id = google_service_account.terraform_sa.name
   public_key_type    = "TYPE_X509_PEM_FILE"
+  private_key_type   = "TYPE_GOOGLE_CREDENTIALS_FILE"
+}
+
+resource "local_file" "sa_key_file" {
+  content  = google_service_account_key.terraform_sa_key.private_key
+  filename = "terraform-sa-key.txt"
 }
