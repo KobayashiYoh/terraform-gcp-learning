e8749aef11edd7f61344eea981229df8076c78c3
Yo Kobayashi <82624334+KobayashiYoh@users.noreply.github.com>
2025-03-05 19:01:41 +0900

    index.jsを修正

--- modules/functions/functions-project/src/index.js
+++ modules/functions/functions-project/src/index.js
@@ -3,9 +3,16 @@ const axios = require("axios");
 require("dotenv").config();
 
 async function sendToDiscord() {
+  const maskedUrl = process.env.DISCORD_WEBHOOK_URL.replace(
+    /(https:\/\/discord\.com\/api\/webhooks\/)(.*?)(\/.*)/,
+    "$1****/****"
+  );
   try {
     await axios.post(process.env.DISCORD_WEBHOOK_URL, {
-      content: "Hello, World",
+      username: "Google Cloud Billing Alert Bot",
+      avater_url:
+        "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhzMqkpQ7vLUKvumbm6AFwTLQiCe7tlDb2Q0MAiISLsesZHnhj0kbRjB4U3se3UrDIHfIy0hlahyphenhyphenQu-V2tOR2LcV_lX7U8P5a8jtqPYv3Ah4L-JoYi8PhoaoehumGIdp2vrsX0rRyhXqwA/s800/mark_chuui.png",
+      content: "Google Cloudの請求額が予算額を超過！！！！",
     });
     console.log("Message sent to Discord");
   } catch (error) {
@@ -15,5 +22,5 @@ async function sendToDiscord() {
 
 export const helloWorldToDiscord = async (req, res) => {
   await sendToDiscord();
-  res.status(200).send("Message sent");
+  res.status(200).send("Message sent to Discord: " + maskedUrl);
 };
